03a5afdb3986c26255d8f180c2e96d8e5d353bb4
eduardz1 <occhipinti.eduard@icloud.com>
2021-11-12 18:37:41 +0100

    started working on parser

--- LFT_lab/Parser.java
+++ LFT_lab/Parser.java
@@ -17,54 +17,85 @@ public class Parser {
     }
 
     void error(String s) {
-	throw new Error("near line " + lex.line + ": " + s);
+        throw new Error("near line " + lex.line + ": " + s);
     }
 
     void match(int t) {
-	if (look.tag == t) {
-	    if (look.tag != Tag.EOF) move();
-	} else error("syntax error");
+        if (look.tag == t) {
+            if (look.tag != Tag.EOF)
+                move();
+        } else
+            error("syntax error");
     }
 
     public void start() {
-	// ... completare ...
-	expr();
-	match(Tag.EOF);
-	// ... completare ...
+        // ... completare ...
+        expr();
+        match(Tag.EOF);
+        // ... completare ...
     }
 
     private void expr() {
-	// ... completare ...
+        term();
+        exprp();
     }
 
     private void exprp() {
-	switch (look.tag) {
-	case '+':
-	// ... completare ...
-	}
+        switch (look.tag) {
+
+            case '+':
+                term();
+                exprp();
+
+            case '-':
+                term();
+                exprp();
+
+            case ' ':
+                // nothing
+
+        }
     }
 
     private void term() {
-        // ... completare ...
+        fact();
+        termp();
     }
 
     private void termp() {
-        // ... completare ...
+        switch(look.tag){
+
+            case '*':
+                fact();
+                termp();
+
+            case '/':
+                fact();
+                termp();
+
+            case ' ':
+                //nothing
+
+        }
     }
 
     private void fact() {
-        // ... completare ...
+        expr();
+        // or
+        match(Tag.NUM);
     }
-		
+
     public static void main(String[] args) {
         Lexer lex = new Lexer();
-        String path = "...path..."; // il percorso del file da leggere
+        String path = "C:\\Users\\occhi\\Github\\university\\LFT_lab\\prova.txt"; // il percorso del file da leggere
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
             Parser parser = new Parser(lex, br);
             parser.start();
             System.out.println("Input OK");
             br.close();
-        } catch (IOException e) {e.printStackTrace();}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
