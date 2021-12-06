9f5eac0c8ed39aaf215f16d8810f9c3f65e2dc5b
eduardz1 <54178827+eduardz1@users.noreply.github.com>
2021-12-06 12:53:12 +0100

    changes at Translator

--- LFT_lab/Translator.java
+++ LFT_lab/Translator.java
@@ -153,7 +153,13 @@ public class Translator {
         }
     }
 
+    public void statlistp(){
+
+    }
+
     public void stat(int lnext_statlist) {
+        code.emitLabel(code.newLabel());
+
         switch (look.tag) {
 
             case Tag.ASSIGN:
@@ -174,50 +180,271 @@ public class Translator {
             case Tag.READ:
                 match(Tag.READ);
                 match('(');
-                if (look.tag == Tag.ID) {
-                    int id_addr = st.lookupAddress(((Word) look).lexeme);
-                    if (id_addr == -1) {
-                        id_addr = count;
-                        st.insert(((Word) look).lexeme, count++);
-                    }
-                    match(Tag.ID);
-                    match(')');
-                    code.emit(OpCode.invokestatic, 0);
-                    code.emit(OpCode.istore, id_addr);
-                }
-                error("Error in grammar (stat) after read( with " + look);
+                idlist();
+                match(')');
+                
+            case Tag.IF:
+                match(Tag.IF);
+                match(Tag.LPT);
+                bexpr();
+                match(Tag.RPT);
+                stat();
+                statp();
+                break;
 
+            case '{':
+                match(Tag.LPG);
+                statlist();
+                match(Tag.RPG);
+                break;
+                
             default:
                 error("Error in stat");
+        }
+    }
+
+    public void statp(){
+        switch(look.tag){
+            case Tag.END:
+                match(Tag.END);
+
+            case Tag.ELSE:
+                {
+                    int label = code.newLabel();
+                    match(Tag.ELSE);
+                    code.emitLabel(OpCode.label, label);
+                    stat(label);
+                    match(Tag.END);
+                    break;
+                }
+
+            default:
+                error("Error in statp()");
         }
     }
 
     private void idlist(int lnext_idlist) {
         switch (look.tag) {
             case Tag.ID:
-                int id_addr = st.lookupAddress(((Word) look).lexeme);
+                {int id_addr = st.lookupAddress(((Word) look).lexeme);
                 if (id_addr == -1) {
                     id_addr = count;
                     st.insert(((Word) look).lexeme, count++);
                 }
                 match(Tag.ID);
+                break;}
 
             default:
                 error("Error in idlist");
         }
     }
 
+    private void idlistp(){
+        switch (look.tag) {
+            // GUIDA[<idlistp> := ,ID<idlistp>] = {,}
+            case ',':
+                {
+                    match(Tag.COM);
+
+                    int id_addr = st.lookupAddress((Word)look);
+                    if(id_addr == -1)
+                        error("Error in expr() : identifier not defined");
+                    code.emit(OpCode.iload, id_addr);
+                    match(Tag.ID);
+        
+                    idlistp();
+                    break;
+                }
+
+            /*
+             * GUIDA[<idlistp> := ε] = FOLLOW[<idlistp>]
+             * FOLLOW[<idlistp>] = {EOF} U {;} U {}} U {end} U {else} U {)}
+             */
+            case -1:
+                break;
+
+            case ';':
+                break;
+
+            case '}':
+                break;
+
+            case Tag.END:
+                break;
+
+            case Tag.ELSE:
+                break;
+
+            case ')':
+                break;
+
+            default:
+                error("Error in idlistp");
+
+        }
+    }
+
+    private void bexpr(int expr_label){
+        switch(look.tag){
+            case Tag.RELOP:
+                {
+                    switch(((Word)look).lexeme){
+                        case "or":
+                            code.emit(OpCode.if_icmpne, expr_label);
+                            break;
+
+                        case "and":
+                            code.emit(OpCode.if_icmpne, expr_label);
+                            break;
+
+                        case "lt":
+                            code.emit(OpCode.if_icmpne, expr_label);
+                            break;
+
+                        case "gt":
+                            code.emit(OpCode.if_icmpne, expr_label);
+                            break;
+
+                        case "eq":
+                            code.emit(OpCode.if_icmpne, expr_label);
+                            break;
+
+                        case "le":
+                            code.emit(OpCode.if_icmpne, expr_label);
+                            break;
+
+                        case "ne":
+                            code.emit(OpCode.if_icmpne, expr_label);
+                            break;
+                        
+                        case "ge":
+                            code.emit(OpCode.if_icmpne, expr_label);
+                            break;
+
+                        default:
+                            error("Error in Word.java RELOP definition");
+                    }
+                    match(Tag.RELOP);
+                    expr();
+                    expr();
+                    break;
+                }
+
+            default:
+                error("Error in bexpr()");
+        }
+    }
+
     private void expr() {
         switch (look.tag) {
-            // ... completare ...
+            case '+':
+                {
+                    match(Tag.SUM);
+                    match(Tag.LPT);
+                
+                    int counter_operators;
+                    counter_operators = exprlist();
+                    while(counter_operators > 0)
+                        code.emit(OpCode.imul);
+
+                    match(Tag.RPT);
+                    break;
+                }
+
             case '-':
                 match('-');
                 expr();
                 expr();
                 code.emit(OpCode.isub);
                 break;
-            // ... completare ...
+            
+            case '*':
+                {
+                    match(Tag.MUL);
+                    match(Tag.LPT);
+                
+                    int counter_operators;
+                    counter_operators = exprlist();
+                    while(counter_operators > 0)
+                        code.emit(OpCode.imul);
+
+                    match(Tag.RPT);
+                    break;
+                }
+
+            case '/':
+                match('/');
+                expr();
+                expr();
+                code.emit(OpCode.isub);
+                break;
+
+            case Tag.NUM:
+                code.emit(OpCode.ldc,((NumberTok)look).value);
+                match(Tag.NUM);
+                break;
+
+            case Tag.ID:
+                {
+                    int id_addr = st.lookupAddress((Word)look);
+                    if(id_addr == -1)
+                        error("Error in expr() : identifier not defined");
+                    code.emit(OpCode.iload, id_addr);
+                    match(Tag.ID);
+                    break;
+                }
+
+            default:
+                error("Error in expr()");
         }
+    }
+
+    private int exprlist(){
+        // return 0 when only one operand is present after + - * /
+        switch (look.tag) {
+            /*
+             * GUIDA[<exprlist> := <expr><exprlistp>] = FIRST[<expr>]
+             * FIRST[<expr>] = {+} U {-} U {*} U {/} U {NUM} U {ID}
+             */
+            case '+':
+                // same as before we recognize the tags but avoid matching any of them
+                expr();
+                exprlistp();
+                break;
+
+            case '-':
+                expr();
+                exprlistp();
+                break;
+
+            case '*':
+                expr();
+                exprlistp();
+                break;
+
+            case '/':
+                expr();
+                exprlistp();
+                break;
+
+            case Tag.NUM:
+                expr();
+                exprlistp();
+                break;
+
+            case Tag.ID:
+                expr();
+                exprlistp();
+                break;
+
+            default:
+                error("Error in exprlist");
+
+        }
+    }
+
+    private void exprlistp(){
+
     }
     // ... completare ...
 }
