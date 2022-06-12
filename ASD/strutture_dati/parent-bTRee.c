fa031f9250ed1fbfb48d31e75b9651bad3937822
Eduard Occhipinti <occhipinti.eduard@icloud.com>
2022-06-12 21:22:59 +0200

    niente

--- ASD/strutture_dati/parent-bTRee.c
+++ ASD/strutture_dati/parent-bTRee.c
@@ -1,6 +1,18 @@
 #include "headers/parent-bTRee.h"
 
-int main ()
+/*
+ * Sia dato un albero T (non vuoto), rappresentato con puntatori child e sibling, nel quale ogni
+ * nodo è etichettato con un numero intero. Si dia un algoritmo che restituisca true se l’etichetta di ogni nodo
+ * che ha almeno un figlio è uguale alla somma delle etichette dei figli, e false altrimenti.
+ */
+bool nodeEqualsToChildSum(btree t)
+{
+    if(t == NULL) return true;
+
+    
+}
+
+int main()
 {
     return 0;
 }
