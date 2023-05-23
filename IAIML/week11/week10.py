9b8da2482f72cbea831c06273c8b92fa9a442983
Eduard Occhipinti <occhipinti.eduard@icloud.com>
2023-05-23 14:10:59 +0200

    formatting

--- IAIML/week11/week10.py
+++ IAIML/week11/week10.py
@@ -2,8 +2,8 @@
 Module for programming exercises week 10.
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
 
@@ -26,8 +26,16 @@ def generateData(n=100):
     x2 = np.random.uniform(low=0, high=1, size=(n, 1)).astype(np.float32)
 
     y = np.empty((n, 2), dtype=float)
-    y[:, 0] = np.sin(x1).flatten() + np.random.normal(loc=0, scale=0.05, size=n) + x2.flatten()
-    y[:, 1] = np.cos(x1).flatten() + np.random.normal(loc=0, scale=0.05, size=n) + x2.flatten()
+    y[:, 0] = (
+        np.sin(x1).flatten()
+        + np.random.normal(loc=0, scale=0.05, size=n)
+        + x2.flatten()
+    )
+    y[:, 1] = (
+        np.cos(x1).flatten()
+        + np.random.normal(loc=0, scale=0.05, size=n)
+        + x2.flatten()
+    )
 
     return (np.concatenate((x1, x2), axis=1), y)
 
@@ -37,8 +45,8 @@ def set_axis_style(ax, n_layers):
     Helper function. Adapted from:
     https://matplotlib.org/3.1.1/gallery/statistics/customized_violin.html#sphx-glr-gallery-statistics-customized-violin-py
     """
-    ax.get_xaxis().set_tick_params(direction='out')
-    ax.xaxis.set_ticks_position('bottom')
+    ax.get_xaxis().set_tick_params(direction="out")
+    ax.xaxis.set_ticks_position("bottom")
     ax.set_xticks(np.arange(1, n_layers + 1))
     ax.set_xlim(0.25, n_layers + 0.75)
 
@@ -61,11 +69,11 @@ def plot_grad(model):
     for loss_value in loss_list:
         grads_list.append(tape.gradient(loss_value, model.trainable_variables))
 
-    del tape # Tape is set to persisten so we explicitly drop it here.
+    del tape  # Tape is set to persisten so we explicitly drop it here.
 
     grad_data = []
     for i, layer_grads in enumerate(zip(*grads_list)):
-        if i%2 == 1:
+        if i % 2 == 1:
             continue
 
         grad_list_flat = np.array([grad for grad in layer_grads]).flatten()
--- IAIML/week12/policies.py
+++ IAIML/week12/policies.py
@@ -1,42 +1,44 @@
-
+import constants as const
 import numpy as np
 
-import constants as const
 
 def policy_random(state):
     return np.random.choice(const.actions)
 
+
 def policy1(state):
-    if(state.y==3):
+    if state.y == 3:
         return const.DOWN
-    else:
-        return const.RIGHT
-    
-def policy2(state):
-    if(state.y==0):
-        if(state.x!=3):
-            return const.DOWN
-        else:
-            return const.RIGHT
-    
-    elif(state.y==1):
-        if (state.x!=0):
-            return const.UP
-        else:
-            return const.RIGHT
-    
-    elif(state.y==3):
-        return const.DOWN
-    
     else:
         return const.RIGHT
 
-def policy_epsilon1(state,eps=0.2):
-    if (np.random.random() < eps):
-        return np.random.choice(const.actions)
-    
+
+def policy2(state):
+    if state.y == 0:
+        if state.x != 3:
+            return const.DOWN
+        else:
+            return const.RIGHT
+
+    elif state.y == 1:
+        if state.x != 0:
+            return const.UP
+        else:
+            return const.RIGHT
+
+    elif state.y == 3:
+        return const.DOWN
+
     else:
-        if(state.y==3):
+        return const.RIGHT
+
+
+def policy_epsilon1(state, eps=0.2):
+    if np.random.random() < eps:
+        return np.random.choice(const.actions)
+
+    else:
+        if state.y == 3:
             return const.DOWN
         else:
             return const.RIGHT
